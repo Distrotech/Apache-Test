4d9907b4c89dd7643a0b4457258133922d1e8a0e
torsten <torsten@13f79535-47bb-0310-9956-ffa450edef68>
2013-05-26 14:32:05 +0000

    Fix a while(each %hash) loop that modifies the hash it iterates over. This has been documented to lead to undefined behavior for a long time and with perl 5.18 it generates a warning which we turn into fatal.
    git-svn-id: https://svn.apache.org/repos/asf/perl/Apache-Test/tags/1_37@1486414 13f79535-47bb-0310-9956-ffa450edef68

--- lib/Apache/TestSSLCA.pm
+++ lib/Apache/TestSSLCA.pm
@@ -100,10 +100,10 @@ my $cert_dn = {
 };
 
 #generate DSA versions of the server certs/keys
-while (my($key, $val) = each %$cert_dn) {
+for my $key (keys %$cert_dn) {
     next unless $key =~ /^server/;
     my $name = join '_', $key, 'dsa';
-    $cert_dn->{$name} = { %$val }; #copy
+    $cert_dn->{$name} = { %{$cert_dn->{$key}} }; #copy
     $cert_dn->{$name}->{OU} =~ s/rsa/dsa/;
 }
 
